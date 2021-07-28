b4a36eb331e2b89cc1344c681a74bec107db190c
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-07-28 19:07:18 -0300

    Adiciona exercicio2

---
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.01/exercicios/ex2/script.js
@@ -0,0 +1,16 @@
+
+let a = 5;
+let b = 10;
+
+if (a>b) {
+
+    console.log("a é maior que b");
+
+}else if (a<b) {
+
+    console.log("a é menor que b");
+
+}else {
+
+    console.log("a é igual a b");
+}
