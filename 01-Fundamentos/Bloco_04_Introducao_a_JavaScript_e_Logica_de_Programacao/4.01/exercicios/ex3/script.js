3b30a251f9358243714748455472a61678f29aff
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-07-28 19:17:11 -0300

    Adiciona exercicio3

---
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.01/exercicios/ex3/script.js
@@ -0,0 +1,13 @@
+let a = 2;
+let b = 5;
+let c = 5;
+
+if (a > b && a > c) {
+    console.log("O maior número é a: " + a);
+}else if (b>a && b>c) {
+    console.log("O maior número 'b: " + b);
+}else if (c>a && c>b) {
+    console.log("O maior número é c: "+ c)
+}else {
+    console.log("Não existe somente um número maior")
+}
