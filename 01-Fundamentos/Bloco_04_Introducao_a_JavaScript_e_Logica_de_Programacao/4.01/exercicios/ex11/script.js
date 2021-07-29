afd8a488eaa8ec84409c9d4f7143ad3b71ecf67c
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-07-29 13:40:18 -0300

    Adiciona exercicio 11

---
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.01/exercicios/ex11/script.js
@@ -0,0 +1,31 @@
+let salarioBruto = 10000;
+let aliquotaINSS;
+let aliquotaIR;
+
+if (salarioBruto <= 1556.94) {
+    aliquotaINSS = 0.08 * salarioBruto;
+}else if (salarioBruto <= 2594.92) {
+    aliquotaINSS = 0.09 * salarioBruto;
+}else if (salarioBruto <= 5189.82) {
+    aliquotaINSS = 0.11 * salarioBruto;
+}else {
+    aliquotaINSS = 570.88;
+}
+
+let salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98) {
+    aliquotaIR = 0;
+}else if (salarioBase <= 2826.65) {
+    aliquotaIR = (salarioBase * 0.075) - 142.8;
+}else if (salarioBase <= 3751.05) {
+    aliquotaIR = (salarioBase * 0.15) - 354.8;
+}else if (salarioBase <= 4664.68) {
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+}else {
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
+
+let salarioLiquido =salarioBase - aliquotaIR;
+
+console.log(salarioLiquido);
