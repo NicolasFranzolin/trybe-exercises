100722d39ef2874164a3f6012772a2fecaaf6f1b
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-07-29 12:53:43 -0300

    Adiciona exercicio 9

---
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.01/exercicios/ex9/script.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 2;
+let num3 = 4;
+
+let verificaImpar1 = num1 % 2;
+let verificaImpar2 = num2 % 2;
+let verificaImpar3 = num3 % 2;
+
+if (verificaImpar1 !== 0 || verificaImpar2 !==0 || verificaImpar3 !== 0) {
+    console.log(true);
+}else {
+    console.log(false);
+}
