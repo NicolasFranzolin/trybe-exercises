743f53beca368f6ae694d6480d9e8799f4645339
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-07-30 19:35:32 -0300

    Adiciona exercicios 1 e 2, 3 incompleto

---
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.03/exercicio3.js
@@ -0,0 +1,21 @@
+/* 3- Agora inverta o lado do triângulo. Por exemplo: */
+
+let n = 5;
+let input = "";
+let blankSpaces = "";
+
+
+
+for (let index = 0; index <= n ; index +=1) {
+    console.log(input);
+
+    input += blankSpaces + "*";    
+}
+
+let numeros = [1,2,3,10]
+let soma = 0;
+
+for (let index = 0; index < numeros.length; index += 1) {
+    soma += numeros[index];
+
+}console.log(soma)
