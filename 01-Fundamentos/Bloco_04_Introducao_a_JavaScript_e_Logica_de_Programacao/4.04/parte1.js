b50b597ae8e2b288ee02951962e757a3e9a7a42e
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-03 14:19:48 -0300

    Adiciona atualizaçoes

--- 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.04/parte1.js
+++ 01-Fundamentos/Bloco_04_Introducao_a_JavaScript_e_Logica_de_Programacao/4.04/parte1.js
@@ -27,3 +27,6 @@ for(let key in info) {
 for(let key in info) {
     console.log(info[key]);
 }
+
+/* Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console: */
+
