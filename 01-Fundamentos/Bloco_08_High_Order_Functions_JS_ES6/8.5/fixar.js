d217bc4f645e90ce6a70a3103aa3f53b23dd69e2
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-09-15 14:42:33 -0300

    Adiciona Pasta 8.5

---
+++ 01-Fundamentos/Bloco_08_High_Order_Functions_JS_ES6/8.5/fixar.js
@@ -0,0 +1,49 @@
+/* // Faça uma lista com as suas frutas favoritas
+const specialFruit = ['pêra', 'uva', 'maçã'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['salada', 'mista', 'hehe'];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+  const merge = [...fruit, ...additional];
+  return merge;
+};
+
+console.log(fruitSalad(specialFruit, additionalItens)); */
+/* 
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+const [saudacao, realizaSaudacao] = saudacoes;
+
+console.log (realizaSaudacao(saudacao)); // Olá */
+
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+
+/* let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida, comida); */
+
+// 3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares .
+
+// let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// [,,, ...numerosPares] = numerosPares;
+
+// console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+
+
